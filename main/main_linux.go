d10d6c73dd7b3e995f408b5909b814adf4500dbe
Fernando Fragateiro <72581331+FernasFragas@users.noreply.github.com>
2023-06-14 19:55:55 +0100

    finsih container in  go for linux

--- main/main_linux.go
+++ main/main_linux.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strconv"
 	"syscall"
 )
 
@@ -23,13 +26,14 @@ func main() {
 
 func run() {
 	fmt.Printf("Running %v\n", os.Args[2:])
-	cmd := exec.Command("/proc/self/exe", "child", os.Args[2], os.Args[3:]...)
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS,
 	}
 
 	cmd.Run()
@@ -37,16 +41,30 @@ func run() {
 
 func child() {
 	fmt.Printf("Running %v\n", os.Args[2:])
+
+	cg()
+
+	syscall.Sethostname([]byte("container"))
+	syscall.Chroot("/home/fernas/ubuntufs")
+	os.Chdir("/")
+	syscall.Mount("proc", "proc", "proc", 0, "")
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
-	}
-
-	syscall.Sethostname([]byte("container"))
-
 	cmd.Run()
+
+	syscall.Unmount("proc", 0)
+}
+
+func cg() {
+	cgroups := "/sys/fs/cgroup/"
+	pids := filepath.Join(cgroups, "pids")
+	os.Mkdir(filepath.Join(pids, "fernas"), 0755)
+	//:() { : | : & }; : create infinite processes
+	ioutil.WriteFile(filepath.Join(pids, "fernas/pids.max"), []byte("20"), 0700)
+	// Removes the new cgroup in place after the container exits
+	ioutil.WriteFile(filepath.Join(pids, "fernas/notify_on_release"), []byte("1"), 0700)
+	ioutil.WriteFile(filepath.Join(pids, "fernas/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700)
 }
